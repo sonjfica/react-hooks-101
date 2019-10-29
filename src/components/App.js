ff54dd09fedc1ca81ce53a2cf3a639b43e4f8ea6
髙橋次郎 <sonjfica.work@gmail.com>
2019-10-29 13:42:51 +0900

    implement all events deletion feature

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,14 @@ const App = () => {
     setBody('')
   }
 
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除してよろしいですか？')
+    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' })
+  }
+
+  const unCreatable = title === '' || body === ''
+
   return (
     <div className='container-fluid'>
       <h4>イベント作成フォーム</h4>
@@ -29,8 +37,8 @@ const App = () => {
             className='form-control'
             id='formEventTitle'
             value={title}
-            onChange={e => setTitle(e.target.value)
-          }/>
+            onChange={e => setTitle(e.target.value)}
+          />
         </div>
 
         <div className='form-gruop'>
@@ -42,8 +50,16 @@ const App = () => {
             onChange={e => setBody(e.target.value)}
           />
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てイベントを削除する</button>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >イベントを作成する</button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >全てのイベントを削除する</button>
       </form>
        <h4>イベント一覧</h4>
        <table className="table table-hover">
